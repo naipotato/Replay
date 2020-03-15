e74f5a53dfef38b71e2bd49b1c453b7f029bd725
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-03-14 22:17:04 -0300

    Renamed from postinstall.py to post_install.py

--- build-aux/post_install.py
+++ build-aux/post_install.py
@@ -7,13 +7,10 @@ prefix = environ.get ('MESON_INSTALL_PREFIX', '/usr/local')
 datadir = path.join (prefix, 'share')
 destdir = environ.get ('DESTDIR', '')
 
-# Package managers set this so we don't need to run
 if not destdir:
     print ('Updating icon cache...')
-    call (['gtk-update-icon-cache', '-qtf', path.join (datadir, 'icons', 'hicolor')])
-
-    print ('Updating desktop database...')
-    call (['update-desktop-database', '-q', path.join (datadir, 'applications')])
+    call (['gtk-update-icon-cache', '-qtf', path.join (datadir, 'icons',
+        'hicolor')])
 
     print ('Compiling GSettings schemas...')
     call (['glib-compile-schemas', path.join (datadir, 'glib-2.0', 'schemas')])
