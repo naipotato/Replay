95a94a3cb44c1b7d42e5c8a1adc066349c7b8bfc
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-09-28 19:48:57 -0300

    Build Aux: Added script to convert from YAML to JSON

---
+++ build-aux/flatpak/yaml-to-json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+YAML to JSON Python script
+
+Highly inspired on https://gitlab.gnome.org/GNOME/geary/-/blob/mainline/build-aux/yaml_to_json.py
+"""
+
+import os, sys, yaml, json
+
+_, filename, output, *_ = sys.argv
+
+with open(filename) as file:
+    obj = yaml.safe_load(file)
+
+comment = '/* Automatically generated from {}, do not modify. */'.format (os.path.basename(output))
+
+with open(output, 'w') as file:
+    print(comment, file=file)
+    json.dump(obj, file, indent=2)
